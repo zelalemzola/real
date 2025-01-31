b7c5e096d2e9602189070f511bdf01f413e8d612
zelalemzola <tzelalemtesfaye@gmail.com>
2025-01-31 08:35:07 +0300

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='py-3 w-full flex items-center justify-between fixed z-20  mb-20 px-5 md:px-20 bg-white'>
       <h1 className="text-2xl font-bold ">Maya</h1>
-      <Link href='/admin/properties' className='bg-primary text-white rounded-2xl px-4 py-2 hover:shadow-2xl'>Admin Panel</Link>
+      <Link href='/admin/properties' className='bg-primary text-white rounded-lg px-4 py-2 hover:shadow-2xl'>Admin Panel</Link>
     </div>
   )
 }
